3928c394ea1b166faaf0f8461da7c033eccfdbdc
root <root@comensal>
2024-06-22 13:24:00 -0300

    adding

--- vecadd.py
+++ vecadd.py
@@ -1,36 +1,33 @@
 import triton
 import triton.language as tl
+import torch
 
 @triton.jit
-def softmax(Y, stride_ym, stride_yn, X, stride_xm, stride_xn, M, N):
-    # row index
-    m = tl.program_id(0)
-    # col indices
-    # this specific kernel only works for matrices that 
-    # have less than BLOCK_SIZE columns
-    BLOCK_SIZE = 1024
-    n = tl.arange(0, 1024) # has to be a constant value
-    # the memory address of all the elements
-    # that we want to load can be computed as follows
-    X = X + m * stride_xm + n * stride_xn
-    # load input data; pad out-of-bounds elements with 0 
-    x = tl.load(X, mask=n < N, other=-float('inf'))
-    # compute numerically-stable softmax
-    z = x - tl.max(x, axis=0)
-    num = tl.exp(z)
-    denom = tl.sum(num, axis=0)
-    y = num / denom
-    # write back to Y
-    Y = Y + m * stride_ym + n * stride_yn
-    tl.store(Y, y, mask=n < N)
+def add_kernel(x_ptr, y_ptr, output_ptr, N, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(axis=0) #identifying which program we're running
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < N
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
 
-import torch
-# Allocate input/output tensors
-X = torch.normal(0, 1, size=(583, 931), device='cuda')
-Y = torch.empty_like(X)
-# SPMD launch grid
-grid = (X.shape[0], )
-# enqueue GPU kernel
-softmax[grid](Y, Y.stride(0), Y.stride(1), 
-              X, X.stride(0), X.stride(1),
-              X.shape[0]    , X.shape[1])
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x)
+    assert x.is_cuda and y.is_cuda and output.is_cuda
+    N = output.numel()
+    #specifying how many iterations to go over the elements
+    grid = lambda meta: (triton.cdiv(N, meta['BLOCK_SIZE']),)
+    add_kernel[grid](x, y, output, N, BLOCK_SIZE=1024)
+    return output
+
+def main():
+    a = torch.Tensor([3., 2., 4., 2., 1.])
+    a = a.to(device=torch.device('cuda'))
+    b = torch.Tensor([4., 2., 1., 4., 0.])
+    b = b.to(device=torch.device('cuda'))
+    print(add(a, b))
+
+if __name__ == '__main__':
+    main()
